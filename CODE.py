badbecd9893a9331a41c7ace77b6a149718aaadb
blipdip <retr0.deep.00@gmail.com>
2022-10-28 05:20:14 +0530

    commit1

---
+++ CODE.py
@@ -0,0 +1,103 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import RMSprop,SGD,Adam
+from tensorflow.keras import optimizers
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+from PIL import Image
+sys.modules['Image'] = Image 
+import cv2
+tf.compat.v1.set_random_seed(2019)
+
+rows, cols = 50,50
+input_shape = (rows,cols,1)
+
+train = ImageDataGenerator(rescale= 1/255)
+validation = ImageDataGenerator(rescale= 1/255)
+test = ImageDataGenerator(rescale= 1/255)
+train_dataset = train.flow_from_directory(r"C:\Users\retr0\Desktop\CCR\new_dat\Train",target_size=(50,50), batch_size=30, class_mode = 'categorical', color_mode = 'grayscale')
+validation_dataset = validation.flow_from_directory(r"C:\Users\retr0\Desktop\CCR\new_dat\Validation",target_size=(50,50), batch_size=30, class_mode = 'categorical', color_mode = 'grayscale')
+test_dataset = test.flow_from_directory(r"C:\Users\retr0\Desktop\CCR\new_dat\Test",target_size=(50,50), batch_size=30, class_mode = 'categorical', color_mode = 'grayscale')
+
+model2 = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(6,(3,3),activation='relu', input_shape=input_shape),
+    tf.keras.layers.AveragePooling2D(),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(16,(3,3),activation='relu'),
+    tf.keras.layers.AveragePooling2D(),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(300, activation='relu'),
+    tf.keras.layers.Dense(120, activation='relu'),
+    tf.keras.layers.Dense(84,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(3,activation='softmax')
+    ])
+
+
+
+
+
+
+
+#model2 = tf.keras.models.Sequential([
+#    tf.keras.layers.Conv2D(16,(3,3),activation = "relu" , input_shape = input_shape) ,
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(32,(3,3),activation = "relu") ,  
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(64,(3,3),activation = "relu") ,  
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(128,(3,3),activation = "relu"),  
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(256,(3,3),activation = "relu"),
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(512,(3,3),activation = "relu"),
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Conv2D(1024,(3,3),activation = "relu"),
+#    tf.keras.layers.MaxPooling2D(2,2),
+#    tf.keras.layers.Flatten(), 
+#    tf.keras.layers.Dense(256,activation="relu"),      #Hidden layer
+#    tf.keras.layers.Dropout(0.3),
+#    tf.keras.layers.Dense(225,activation ="relu"),
+#    tf.keras.layers.Dropout(0.2),
+#    tf.keras.layers.Dense(200,activation="relu"),
+#    tf.keras.layers.Dropout(0.1),
+#    tf.keras.layers.Dense(150,activation ="relu"),
+#    tf.keras.layers.Dropout(0.1),
+#    tf.keras.layers.Dense(125,activation ="relu"),
+#    tf.keras.layers.Dropout(0.1),
+#    tf.keras.layers.Dense(75,activation ="relu"),
+#    tf.keras.layers.Dropout(0.1),
+#    tf.keras.layers.Dense(25,activation ="relu"),
+#    tf.keras.layers.Dropout(0.1),
+    #tf.keras.layers.Dense(15,activation ="relu"),
+    #tf.keras.layers.Dropout(0.1),
+    #tf.keras.layers.Dense(8,activation ="relu"),
+    #tf.keras.layers.Dropout(0.1),
+ #   tf.keras.layers.Dense(3,activation = "softmax")   #Output Layer
+#])
+
+model2.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+print("")
+model2.summary()
+
+bs=80
+epochs=10
+
+model2_fit = model2.fit(train_dataset,
+                       validation_data=test_dataset,
+                       steps_per_epoch=8500 // bs,
+                       epochs=epochs,
+                       validation_steps=600 // bs)
+
+validation_dataset.reset()
+
+model2.evaluate(validation_dataset)
+
+print("")
+print("Total GPUs available: ", len(tf.config.list_physical_devices('GPU')))
+print("GPU used is Nvidia GTX 1650")
---
+++ pre-req.bat
@@ -0,0 +1,10 @@
+@echo off
+
+Rem run this file to install all the pre-requisites but first install conda and python using their respective installers
+
+
+conda install -c conda-forge cudatoolkit cudnn
+pip install tensorflow-gpu
+pip install scikit-learn
+pip install matplotlib
+pip install opencv-python
